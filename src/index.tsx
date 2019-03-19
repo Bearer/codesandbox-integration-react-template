2dc1f3c39d22532652b34aab450594686e14d4a9
Tanguy Antoine <antoine@bearer.sh>
2019-03-19 11:37:47 +0100

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import { render } from "react-dom";
+import { Bearer } from "@bearer/react";
+
+import { CLIENT_ID } from "./constants";
+import { Connect } from "./integrations.tsx";
+import "./styles.css";
+
+function App() {
+  return (
+    <Bearer clientId={CLIENT_ID}>
+      <div className="App">
+        <h1>Welcome to Bearer</h1>
+        <p>Start editing integration.tsx</p>
+        <Connect
+          render={({ connect }) => {
+            return <button onClick={connect}>Connect</button>;
+          }}
+        />
+      </div>
+    </Bearer>
+  );
+}
+
+const rootElement = document.getElementById("root");
+render(<App />, rootElement);
---
+++ src/integrations.tsx
@@ -0,0 +1,4 @@
+import { factory } from "@bearer/react";
+import { INTEGRATION_NAME } from "./constants";
+
+export const { withFunctionCall, Connect } = factory(INTEGRATION_NAME);
